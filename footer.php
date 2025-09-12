2c09f1f4c583f00291dad93bd61f4a1622bbeced
yumauda <firststep-yumauda@gmail.com>
2025-09-12 16:10:30 +0900

    最初のコミット

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer class="p-footer">
+    <div class="l-inner">
+        <div class="p-footer__content">
+            <div class="p-footer__left">
+                <div class="p-footer__logo">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__home">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_logo.png" alt="株式会社フクダ" width="350" height="67">
+                    </a>
+                </div>
+                <div class="p-footer__address-wrapper">
+                    <p class="p-footer__address">〒699-0551 島根県出雲市斐川町沖洲1080番地<br>
+                        TEL : <a href="tel:0853720201" class="p-footer__tel">0853-72-0201</a> / FAX : 0853-72-0205</p>
+                </div>
+                <div class="p-footer__sns-wrapper">
+                    <div class="p-footer__sns">
+                        <a href="#" class="p-footer__facebook">
+                            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facebook.png" alt="" width="19" height="30">
+                        </a>
+                        <a href="#" class="p-footer__instagram">
+                            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/instagram.png" alt="" width="28" height="32">
+                        </a>
+                    </div>
+                    <div class="p-footer__copy">
+                        <small>©FUKUDA CONSTRUCTION CO.,LTD</small>
+                    </div>
+                </div>
+            </div>
+            <div class="p-footer__right">
+                <ul class="p-footer__lists">
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">フクダの働き方</a>
+                        <ul class="p-footer__small-lists">
+                            <li class="p-footer__small-list">
+                                <a href="#" class="p-footer__small-link">各種制度</a>
+                            </li>
+                            <li class="p-footer__small-list">
+                                <a href="#" class="p-footer__small-link">福利厚生</a>
+                            </li>
+                            <li class="p-footer__small-list">
+                                <a href="#" class="p-footer__small-link">環境づくり</a>
+                            </li>
+                            <li class="p-footer__small-list">
+                                <a href="#" class="p-footer__small-link">育成・キャリア</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">数字で見るFUKUDA</a>
+                        <a href="#" class="p-footer__link">FUKUDAひと図鑑</a>
+                        <a href="#" class="p-footer__link">インターンシップ</a>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">お知らせ</a>
+                        <a href="#" class="p-footer__link">コーポレートサイト</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ front-page.php
@@ -0,0 +1,65 @@
+<?php get_header(); ?>
+<main>
+  <div class="p-mv">
+    <div class="p-mv__img">
+      <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_pc.jpg" alt="一緒に街を作ってみる？" width="1920" height="1080">
+    </div>
+  </div>
+  <section class="p-do">
+    <div class="l-inner">
+      <div class="p-do__content">
+        <div class="p-do__text-wrapper">
+          <p class="p-do__text">「できるかどうか」より、「やってみたいかどうか」。<br>
+            誠実で、ていねいで、自分の役割を果たせる人こそ、<br>
+            この町を支える本当の力になれる。<br>
+            特別じゃなくていい。まっすぐなあなたがいい。<br>
+            “今のあなた”を信じて踏み出せば、<br>
+            ここには一緒に育っていく場所が、仲間がある。</p>
+        </div>
+        <figure class="p-do__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/do_img.png" alt="やってみたいかどうか" width="1920" height="1349">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <div class="section__marquee">
+    <p class="marquee__text">Wanna build something? Wanna build something?</p>
+    <p class="marquee__text">Wanna build something? Wanna build something?</p>
+    <p class="marquee__text">Wanna build something? Wanna build something?</p>
+  </div>
+  <div class="p-submit">
+    <div class="p-submit__inner">
+      <div class="p-submit__btn-wrapper">
+        <a href="#" class="p-submit__btn">
+          <div class="p-submit__btn-detail">
+            <p class="p-submit__btn-title">INTERNSHIP</p>
+            <p class="p-submit__btn-text">“なんとなく興味ある”でOK！<br>フクダはあなたの「第一歩」を応援します。</p>
+          </div>
+          <div class="p-submit__btn-icon">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/submit_icon.png" alt="INTERNSHIP" width="120" height="120">
+          </div>
+        </a>
+        <a href="#" class="p-submit__btn p-submit__btn-entry">
+          <div class="p-submit__btn-detail">
+            <p class="p-submit__btn-title">entry</p>
+            <p class="p-submit__btn-text">“なんとなく興味ある”でOK！<br>フクダはあなたの「第一歩」を応援します。</p>
+          </div>
+          <div class="p-submit__btn-icon">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/submit_icon.png" alt="INTERNSHIP" width="120" height="120">
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="p-contact-btn">
+    <div class="l-inner">
+      <div class="p-contact-btn__content">
+        <a href="#" class="p-contact-btn__link">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/contact_btn_img.png" alt="" width="594" height="94">
+        </a>
+      </div>
+    </div>
+  </div>
+
+</main>
+<?php get_footer() ?>
