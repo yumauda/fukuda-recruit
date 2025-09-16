c48bddb73aa0116106f70eb784a3c8e2ae817114
yumauda <firststep-yumauda@gmail.com>
2025-09-16 11:56:00 +0900

    cultureページ途中

---
+++ page-culture.php
@@ -0,0 +1,152 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--culture">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <h2 class="p-page-mv__title">
+            <span class="p-page-mv__title-en">Culture</span>
+            <span class="p-page-mv__title-ja">フクダの働き方</span>
+          </h2>
+        </div>
+        <figure class="p-page-mv__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_culture.png" alt="" width="1920" height="1080">
+        </figure>
+        <div class="p-page-mv__text-wrapper">
+          <p class="p-page-mv__text">働きやすさを支えてくれる制度があること。<br>
+            それは、「安心して働き続けられる会社かどうか」の大切な指標。<br>
+            福利厚生や研修制度、資格取得の支援。<br>
+            どれも社員一人ひとりの成長と、これからの人生を見据えて整えています。<br>
+            フクダが大切にしているのは、「人」の存在そのもの。<br>
+            制度があるからこそ、未来を描ける。<br>
+            そんな職場を、私たちは本気でつくっています。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="p-scroll-photo p-scroll-photo--culture">
+    <ul class="p-scroll-photo__slider">
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll1.png" alt="金属の未来をリファイン" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll2.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll3.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll4.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll5.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll1.png" alt="" width="431" height="38">
+      </li>
+    </ul>
+    <ul class="p-scroll-photo__slider">
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll2.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll3.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll4.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll5.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll1.png" alt="" width="431" height="38">
+      </li>
+      <li class="p-scroll-photo__slide">
+        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/culture_scroll2.png" alt="" width="431" height="38">
+      </li>
+    </ul>
+  </div>
+  <section class="p-row">
+    <div class="l-inner">
+      <div class="p-row__content">
+        <h3 class="p-row__title-wrapper">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_title_1.png" alt="福利厚生" width="131" height="364">
+        </h3>
+        <div class="p-row__block">
+          <div class="p-row__block-top">
+            <div class="p-row__block-titleWrapper">
+              <h4 class="p-row__block-title">安心して働ける、という土台を大切に。</h4>
+            </div>
+            <div class="p-row__block-textWrapper">
+              <p class="p-row__block-text">毎日を心地よく働けること。<br>
+                そして、もしもの時にも会社がしっかり支えてくれること。<br>
+                フクダでは、暮らしと仕事のバランスを整える各種手当や休暇制度、社会保険など、安心して働ける環境を整えています。<br>
+                制度の“使いやすさ”にもこだわり、社員一人ひとりが長く活躍できる仕組みをつくっています。</p>
+            </div>
+            <ul class="p-row__lists">
+              <li class="p-row__list">
+                <figure class="p-row__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_1.png" alt="研修旅行" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">研修旅行</p>
+                  <p class="p-row__detail-text">2年に1度の社内研修旅行に加え、入社5年目には海外研修旅行も実施。<br>
+                    勤続20年以上の社員には、ご家族との海外旅行を贈る制度もあります。</p>
+                </div>
+              </li>
+              <li class="p-row__list">
+                <figure class="p-row__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_2.png" alt="社内活動" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">社内活動</p>
+                  <p class="p-row__detail-text">希望者による陸上部や野球部が活動中。マラソン大会や地域との親善試合を通じて、健康づくりと交流を楽しんでいます。</p>
+                </div>
+              </li>
+              <li class="p-row__list">
+                <figure class="p-row__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_3.png" alt="資格取得制度" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">資格取得制度</p>
+                  <p class="p-row__detail-text">資格取得にかかる費用に加え、旅費や宿泊費も会社が負担します。<br>
+                    さらに、社内勉強会を定期的に開き、挑戦する社員をしっかりサポートしています。</p>
+                </div>
+              </li>
+              <li class="p-row__list">
+                <figure class="p-row__img p-row__img--115">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_4.png" alt="メンター制度" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">メンター制度</p>
+                  <p class="p-row__detail-text">本社にて3か月は年の近い先輩社員が基礎教育を行い、その後現場配属後も年の近い社員が付き一緒に写真管理や安全管理、CADなどの使い方を勉強していきます。</p>
+                </div>
+              </li>
+              <li class="p-row__list">
+                <figure class="p-row__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_5.png" alt="ノー残業デー" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">ノー残業デー</p>
+                  <p class="p-row__detail-text">ワークライフバランスの考えに賛同し、その一つの形として毎週水曜日をノー残業デーとして設定しています。</p>
+                </div>
+              </li>
+              <li class="p-row__list">
+                <figure class="p-row__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/row_6.png" alt="時間単位有給" width="100" height="100">
+                </figure>
+                <div class="p-row__detail">
+                  <p class="p-row__detail-title">時間単位有給</p>
+                  <p class="p-row__detail-text">有給休暇を1時間単位から取得できる制度です。子供の送迎や通院、私用などで短時間会社を抜けたいときなどに活用している社員が多いです。</p>
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php get_template_part('includes/submit'); ?>
+</main>
+<?php get_footer() ?>
