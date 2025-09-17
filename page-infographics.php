c79b2605c1dccc16ea22c4de688b7bd938b06c92
yumauda <firststep-yumauda@gmail.com>
2025-09-17 11:44:06 +0900

    infographicsページ

---
+++ page-infographics.php
@@ -0,0 +1,148 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--infographics">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <h2 class="p-page-mv__title">
+            <span class="p-page-mv__title-en">Infographics</span>
+            <span class="p-page-mv__title-ja">数字で見るフクダ</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="p-info">
+    <div class="l-inner">
+      <div class="p-info__content">
+        <div class="p-info__grid">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info_1.png" alt="年齢" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box--transparent">
+            <div class="p-info__box">
+              <figure class="p-info__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info2-1.png" alt="年齢" width="460" height="420">
+              </figure>
+            </div>
+            <div class="p-info__row">
+              <div class="p-info__row-box">
+                <figure class="p-info__row-img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info2-2.png" alt="年齢" width="460" height="420">
+                </figure>
+              </div>
+              <div class="p-info__row-box">
+                <figure class="p-info__row-img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info2-3.png" alt="年齢" width="460" height="420">
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info_3.png" alt="文系理系" width="460" height="420">
+            </figure>
+          </div>
+        </div>
+        <div class="p-info__grid">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info4.png" alt="年齢" width="460" height="420">
+            </figure>
+          </div>
+
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info5.png" alt="採用タイプ" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info6.png" alt="考え事" width="460" height="420">
+            </figure>
+          </div>
+        </div>
+        <div class="p-info__bottom">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info7.png" alt="仕事するなら..." width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__flex-wrapper">
+            <div class="p-info__box">
+              <figure class="p-info__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info8.png" alt="通勤時間は？" width="460" height="420">
+              </figure>
+            </div>
+            <div class="p-info__box">
+              <figure class="p-info__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info9.png" alt="通勤方法は？" width="460" height="420">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div class="p-info__grid">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info10.png" alt="年齢" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info11.png" alt="採用タイプ" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box-column">
+
+            <div class="p-info__box">
+              <figure class="p-info__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info12.png" alt="考え事" width="460" height="420">
+              </figure>
+            </div>
+            <div class="p-info__box">
+              <figure class="p-info__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info13.png" alt="考え事" width="460" height="420">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div class="p-info__grid">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info14.png" alt="年齢" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info15.png" alt="採用タイプ" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info16.png" alt="考え事" width="460" height="420">
+            </figure>
+          </div>
+        </div>
+        <div class="p-info__bottom">
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info17.png" alt="年齢" width="460" height="420">
+            </figure>
+          </div>
+          <div class="p-info__box">
+            <figure class="p-info__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/info18.png" alt="採用タイプ" width="460" height="420">
+            </figure>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <?php get_template_part('includes/submit'); ?>
+</main>
+<?php get_footer() ?>
