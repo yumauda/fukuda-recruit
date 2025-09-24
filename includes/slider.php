946f0d14fedbafeacc7adc89a728493cdf9f61dc
yumauda <firststep-yumauda@gmail.com>
2025-09-24 17:55:32 +0900

    スライダーインクルード化

---
+++ includes/slider.php
@@ -0,0 +1,93 @@
+<section class="p-top-people p-top-people--interview">
+    <div class="l-inner">
+        <div class="p-top-people__content">
+            <div class="p-top-people__main">
+                <h3 class="p-top-people__top-title">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/people_logo_white.png" alt="FUKUDAひと図鑑" width="396" height="239">
+                </h3>
+
+            </div>
+            <div class="p-top-people__swiper p-top-people__swiper--interview">
+                <!-- Slider main container -->
+                <div class="swiper swiper1">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview01'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w509">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_1.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail">
+                                    <h3 class="p-top-people__title">信頼を築き<br>成果を目指すヒト</h3>
+                                    <p class="p-top-people__en">KENSUKE.Y</p>
+                                    <p class="p-top-people__occupation">営業</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview02'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w628">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_2.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail p-top-people__detail--right">
+                                    <h3 class="p-top-people__title">体力と責任感を<br>鍛えるヒト</h3>
+                                    <p class="p-top-people__en">KANAU.M</p>
+                                    <p class="p-top-people__occupation">軌道班</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview04'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w462">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_3.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail p-top-people__detail--right">
+                                    <h3 class="p-top-people__title">教えて学び、<br>頼られるヒト</h3>
+                                    <p class="p-top-people__en">TAYUTO.K</p>
+                                    <p class="p-top-people__occupation">土木</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview06'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w532">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_4.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail p-top-people__detail--right">
+                                    <h3 class="p-top-people__title">効率と工夫で<br>成果を出すヒト</h3>
+                                    <p class="p-top-people__en">JYO.M</p>
+                                    <p class="p-top-people__occupation">土木</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview05'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w470">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_5.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail">
+                                    <h3 class="p-top-people__title">失敗を糧に<br>強くなるヒト</h3>
+                                    <p class="p-top-people__en">KYOTA.I</p>
+                                    <p class="p-top-people__occupation">建築</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="<?php echo home_url('/interview03'); ?>" class="p-top-people__item">
+                                <figure class="p-top-people__img w429">
+                                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/swiper_6.png" alt="" width="499" height="574">
+                                </figure>
+                                <div class="p-top-people__detail p-top-people__detail--429">
+                                    <h3 class="p-top-people__title">前向きに<br>学び続けるヒト</h3>
+                                    <p class="p-top-people__en">REMI.A</p>
+                                    <p class="p-top-people__occupation">総務</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
